b8fdfd1a689faf8c98f5e4ce8c32c2fb18b2c7cd
MrMan <chromestrikeyt@gmail.com>
2022-02-11 16:06:47 -0500

    GDoc Embed
    Cleanup

--- script.js
+++
@@ -1,8 +0,0 @@
-let header = document.getElementById("header")
-
-if (header) {
-    console.log("hi")
-    window.addEventListener("scroll", (e) => {
-        header.style.backgroundPositionY = `${window.scrollY *0.3}px`
-    })
-}
