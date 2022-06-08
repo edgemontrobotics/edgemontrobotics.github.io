3f8041917dbeaf23c40634955115ce1bdd7ee5a1
nikapadia <nilayrkapadia@gmail.com>
2022-06-07 21:07:21 -0400

    Removed AoS import

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,6 @@
     import './global.css';
     import Footer from "../components/footer.svelte";
     import Navbar from "../components/navbar.svelte";
-    import AOS from "../components/aos.svelte";
 </script>
 
 
